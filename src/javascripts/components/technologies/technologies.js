08ee5bce35b238de9414766891ccb96b399d2a3d
Emilee Mitchell <emcakes@gmail.com>
2019-12-24 18:30:00 -0600

    added about and technology icons

---
+++ src/javascripts/components/technologies/technologies.js
@@ -0,0 +1,24 @@
+import utilities from '../../helpers/utilities';
+import './technologies.scss';
+
+const createTechnologies = () => {
+  const domString = `
+          <h2> Technologies </h2>
+          <div class="icons container">
+          <i class="devicon-html5-plain-wordmark"></i>
+          <i class="devicon-css3-plain-wordmark"></i>
+          <i class="devicon-javascript-plain"></i>
+          <i class="devicon-bootstrap-plain-wordmark"></i>
+          <i class="devicon-sass-original"></i>
+          <i class="devicon-jquery-plain-wordmark"></i>
+          <i class="devicon-react-original-wordmark"></i>
+          <i class="devicon-git-plain-wordmark"></i>
+          <i class="devicon-github-plain-wordmark"></i>
+          <i class="devicon-webpack-plain-wordmark"></i>
+          <i class="devicon-visualstudio-plain"></i>
+        </div>
+  `;
+  utilities.printToDom('technologiesPage', domString);
+};
+
+export default { createTechnologies };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -8,43 +8,8 @@ import myProjects from './components/projects/projects';
 import '@fortawesome/fontawesome-free/js/all';
 
 import apiKeys from './helpers/apiKeys.json';
-// const projects = [
-//   {
-//     title: 'Product Cards',
-//     screenshot: 'https://raw.githubusercontent.com/EmileeA/product-cards/master/screenshots/product-cards.png',
-//     description: 'Product cards using html, css, and flexbox for positioning', // A good project description includes 'the what', 'the why', and 'the how'.
-//     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-//     available: true,
-//     url: 'https://github.com/EmileeA', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-//     githubUrl: 'https://github.com/EmileeA/product-cards',
-//   },
-//   {
-//     title: 'Pet Adoption',
-//     screenshot: 'https://raw.githubusercontent.com/EmileeA/pet-adoption/master/screenshots/pet-adoption.png',
-//     description: 'Pet adoption page using html, css, and javascript', // A good project description includes 'the what', 'the why', and 'the how'.
-//     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-//     available: true,
-//     url: 'https://github.com/EmileeA', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-//     githubUrl: 'https://github.com/EmileeA/pet-adoption',
-//   },
-//   {
-//     title: 'Sorting Hat',
-//     screenshot: 'https://raw.githubusercontent.com/EmileeA/sortinghat/master/screenshots/sorting-hat.png',
-//     description: 'Allows you to sort students into one of the harry potter houses.', // A good project description includes 'the what', 'the why', and 'the how'.
-//     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Bootsrap, Version Control with Github',
-//     available: true,
-//     url: 'https://github.com/EmileeA', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-//     githubUrl: 'https://github.com/EmileeA/sortinghat',
-//   },
-//   {
-//     title: 'Sandwich Maker',
-//     screenshot: 'https://raw.githubusercontent.com/EmileeA/sandwich-maker/master/screenshots/Sammich%20-%20On%20Load.png',
-//     description: 'An application allowing users to create a custom sandwich.', // A good project description includes 'the what', 'the why', and 'the how'.
-//     technologiesUsed: 'HTML5, CSS, and Javascript, Bootstrap, ES6 Modules',
-//     available: true,
-//     url: 'https://github.com/EmileeA', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-//     githubUrl: 'https://github.com/EmileeA/sandwich-maker',
-//   },
+import technologies from './components/technologies/technologies';
+
 //   {
 //     title: 'Tamagotchi',
 //     screenshot: 'https://raw.githubusercontent.com/EmileeA/tamagotchi/master/src/screenshots/Tamagotchi%20-%20load.png',
@@ -81,6 +46,7 @@ const init = () => {
   navBar.createNavBar();
   navBar.navClick();
   myProjects.createProjectCards();
+  technologies.createTechnologies();
 };
 
 init();
